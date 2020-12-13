78bfb0dff5318c8df376d622ea94dd6c0064ac2c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-13 11:20:10 +0000

    Deploy Cheriit/ioprojectarchitecture to github.com/Cheriit/ioprojectarchitecture.git:gh-pages

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pl.put.poznan.scenarioqualitychecker.app"},{"l":"pl.put.poznan.scenarioqualitychecker.logic"},{"l":"pl.put.poznan.scenarioqualitychecker.logic.models"},{"l":"pl.put.poznan.scenarioqualitychecker.rest"}]
