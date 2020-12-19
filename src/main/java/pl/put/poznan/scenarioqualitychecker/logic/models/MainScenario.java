0241ae06f478244efc9188dd054537d8080aefb6
Wiktor Górczak <gorczakwiktor@gmail.com>
2020-12-19 18:35:25 +0100

    Update MainScenario.java

--- src/main/java/pl/put/poznan/scenarioqualitychecker/logic/models/MainScenario.java
+++ src/main/java/pl/put/poznan/scenarioqualitychecker/logic/models/MainScenario.java
@@ -37,12 +37,14 @@ public class MainScenario extends Scenario {
 	@Override
 	public Step getLimitedDepthCopy(int depthLimit) {
 		MainScenario result = new MainScenario(this.getContent());
+		
 		result.setId(this.getId());
 		result.setNumber(this.getNumber());
 		result.setHead(this.head);
-		if (depthLimit>0){
-			for(int i=0;i<this.steps.size();i++){
-				Step child = this.steps.get(i).getLimitedDepthCopy(depthLimit-1);
+		
+		if (depthLimit > 0) {
+			for(int i = 0; i < this.steps.size(); i++) {
+				Step child = this.steps.get(i).getLimitedDepthCopy(depthLimit - 1);
 				result.addStep(child);
 			}
 		}
