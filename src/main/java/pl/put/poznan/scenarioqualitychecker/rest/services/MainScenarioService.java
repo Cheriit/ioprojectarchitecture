e9ba3faed00d31be353eaddad9d039ab23508327
wiktorgorczak <gorczakwiktor@gmail.com>
2020-12-19 15:26:56 +0100

    add put

--- src/main/java/pl/put/poznan/scenarioqualitychecker/rest/services/MainScenarioService.java
+++ src/main/java/pl/put/poznan/scenarioqualitychecker/rest/services/MainScenarioService.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import pl.put.poznan.scenarioqualitychecker.logic.models.MainScenario;
+import pl.put.poznan.scenarioqualitychecker.logic.models.Scenario;
+import pl.put.poznan.scenarioqualitychecker.logic.models.Step;
 import pl.put.poznan.scenarioqualitychecker.persistence.repositories.MainScenarioRepository;
 
 /**
@@ -45,7 +47,7 @@ public class MainScenarioService {
 	@Transactional(readOnly = false)
 	public void create(MainScenario scenario) {
 		mainScenarioRepository.save(scenario);
-		logger.debug("Creating scenario...");
+		logger.debug("Scenario created.");
 	}
 	
 	/**
@@ -55,7 +57,7 @@ public class MainScenarioService {
 	@Transactional(readOnly = false)
 	public void delete(MainScenario scenario) {
 		mainScenarioRepository.delete(scenario);
-		logger.debug("Deleting scenario...");
+		logger.debug("Scenario deleted.");
 	}
 	
 	/**
@@ -65,9 +67,27 @@ public class MainScenarioService {
 	 */
 	@Transactional(readOnly = false)
 	public void update(MainScenario oldScenario, MainScenario newScenario) {
-		//TODO: Implement
-		logger.debug("Updating scenario...");
+		mainScenarioRepository.delete(oldScenario);
+		mainScenarioRepository.save(newScenario);
+		logger.debug("Scenario updated.");
 	}
+	
+	//TODO: If we ever need to make it more efficient, the first thing to do
+	//		would be making the update operation more detailed
+//	@Transactional(readOnly = false)
+//	public void updateStep(Step oldStep, Step newStep) {
+//		oldStep.setContent(newStep.getContent());
+//		oldStep.setNumber(newStep.getNumber());
+//		
+//		if(oldStep instanceof Scenario && newStep instanceof Scenario) {
+//			Scenario oldScenario = (Scenario) oldStep;
+//			Scenario newScenario = (Scenario) newStep;
+//			
+//			
+//		}
+//			
+//		
+//	}
 
 	/**
 	 * Fetches all scenarios from repository.
